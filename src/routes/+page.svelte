5b01d96437cb6d2dee6a2e60cdcbc38c60dc8624
Pablo <pablo.r.c@live.com>
2023-01-19 01:38:31 +0100

    animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,40 +30,70 @@
   }
 
   let visible = false;
-  const verb = ["Don't Stop", "Enjoy", "Start", "Continue", "Keep"];
+  const verb = ["Don't Stop", "Enjoy", "Start", "Continue", "Keep", "Dont' Cease"];
   const action = ["Running !", "🏃‍♀️🏃‍♀️", "🏃‍♀️", "🏃‍♂️", "🏃‍♂️🏃‍♀️" , "Pushing !", "Showing up !"]
 
   const text = takeRan(verb) + ' ' + takeRan(action)
 
   const noran = ["NOT YET !", "ALMOST THERE", "TOMORROW", "WAIT A BIT", "PATIENCE", 
-  "NOT TODAY", "ALMOST", "NO NO", "NOPE", "PATIENCE BRUH"];
+  "NOT TODAY", "ALMOST", "NO NO", "NOPE", "PATIENCE BRUH", "N😔"];
   const randomAction = takeRan(noran) //Math.floor(Math.random() * action.length);
 
+  
+  function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+
 	onMount(async () => {
-		visible = true
+    visible=true
+    //delay(1000).then(() =>visible = true);
 	});
-
 </script>
-
-
 
 
 <div class="grid grid-cols-1 place-content-center h-full w-full ">
     <div class="card card-glass p-6 m-auto space-y-6">
-      {#if !$isRunningDay}
-      {#if visible}
-        <h1 transition:typewriter class="text-center text-9xl">  YES! </h1>
-      {:else}
-        <h1>  YES! </h1>
-      {/if}
+      {#if $isRunningDay}  
+        
+        <div class="waviy text-center" style="font-size: 40px;">
+        <span style="--i:1"><h1 class="text-9xl">Y</h1></span>
+        <span style="--i:2"><h1 class="text-9xl">E</h1></span>
+        <span style="--i:3"><h1 class="text-9xl">S</h1></span>
+        <span style="--i:4"><h1 class="text-9xl">!</h1></span>
+       </div>
+       <!--
+        {#if visible}
+          <h1 transition:typewriter class="text-center text-9xl waviy">  YES! </h1>
+        {/if}
+      -->
       <h2  class="text-center text-6xl">{text}</h2>
       <h3 class="text-center text-2xl"> {$time} till rest 🛋️ day </h3>
     {:else}     
       <h1 class="text-center text-9xl"> {randomAction} </h1>
       <h2 class="text-center text-6xl"> {$time2} </h2>
       {#if visible}
-        <h3 transition:typewriter class="text-center text-2xl"> till 🏃‍♀️ day </h3> 
+        <h3 transition:typewriter class="text-center text-2xl"> Count down till 🏃‍♀️ day </h3> 
       {/if}
     {/if}
   </div>
 </div>
+
+<style>
+  .waviy {
+    position: relative;
+  }
+  .waviy span {
+    position: relative;
+    display: inline-block;
+    font-size: 40px;
+    color: #fff;
+    text-transform: uppercase;
+    animation: flip 2s 5s;
+    animation-delay: calc(.2s * var(--i))
+  }
+  @keyframes flip {
+    0%,80% {
+      transform: rotateY(360deg) 
+    }
+  }
+</style>
